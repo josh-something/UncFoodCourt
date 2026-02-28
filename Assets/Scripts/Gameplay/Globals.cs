894a0f0f48e0f1f4e1863c98dd02903a285a55ac
josh-something <joshuafranzsabaot@gmail.com>
2026-02-28 22:57:23 +0800

    Update Globals.cs
    Grades enum moved

--- Assets/Scripts/Gameplay/Globals.cs
+++ Assets/Scripts/Gameplay/Globals.cs
@@ -1,13 +1,13 @@
-using System;
+
 using UnityEngine;
 
 public static class Globals
 {
     public static Bounds WorldBounds;
-    public enum Grades
-    {
-        Bad,
-        Good,
-        Perfect
-    }
+}
+public enum Grades
+{
+    Bad,
+    Good,
+    Perfect
 }
